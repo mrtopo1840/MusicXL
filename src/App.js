3b21eac41d0c5041a2ea415eadb4acd2fd1abfe2
mrtopo1840 <juanfelipecortescastrillon@gmail.com>
2022-03-18 14:31:27 -0500

    Configuracion de electron y de el lansamiento dev

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h2>React and electron</h2>
+        <h3>Mi nombre es juan felipe</h3>
+       
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
