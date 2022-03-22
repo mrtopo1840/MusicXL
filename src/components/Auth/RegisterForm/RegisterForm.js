81eca849626792e6a60e0cc282b4cf96dbebe9e8
mrtopo1840 <juanfelipecortescastrillon@gmail.com>
2022-03-21 20:06:11 -0500

    Se agregaron los componentes Auth en page, AuthOptions, LoginForm, RegisterForm en components que se va a utilizar para los formularios de registro o logeo, tambien se agrego un nuevo color en _colors

---
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RegisterForm() {
+  return (
+    <div>
+      <h1>Register Form ...</h1>
+    </div>
+  );
+}
--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+//Importacion de los componentes que vamos a usar
+import AuthOptions from "../../components/Auth/AuthOptions";
+import LoginForm from "../../components/Auth/LoginForm";
+import RegisterForm from "../../components/Auth/RegisterForm";
+//Importacion de las imagenes
+import BackgroundAuth from "../../assets/jpg/background-auth.jpg";
+import LogoNameWith from "../../assets/png/logo-name-white.png";
+
+import "./Auth.scss";
 
 export default function Auth() {
+  const [selectedForm, setSelectedForm] = useState(null);
+
+  //Funcion de manejo de los forms que se van a usar a travez de un
+  // swich
+  const handlerForm = () => {
+    switch (selectedForm) {
+      case "login":
+        return <LoginForm />;
+      case "register":
+        return <RegisterForm />;
+      default:
+        return <AuthOptions />;
+    }
+  };
+
   return (
-    <div>
-      <h2>Iniciar sesion</h2>
-      <h2>Registrar</h2>
+    <div className="auth" style={{ backgroundImage: `url(${BackgroundAuth})` }}>
+      <div className="auth__dark" />
+      <div className="auth__box">
+        <div className="auth__box-logo">
+          <img src={LogoNameWith} alt="MusicXL" />
+        </div>
+        {handlerForm()}
+      </div>
     </div>
   );
 }
